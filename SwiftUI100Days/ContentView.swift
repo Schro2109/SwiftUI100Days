962e3851bcaed5f3787b60f6dab3b839c5abb426
Schro2109 <bloodparty297@gmail.com>
2026-09-14 15:36:49 +0900

    add: Dictionary

--- SwiftUI100Days/ContentView.swift
+++ SwiftUI100Days/ContentView.swift
@@ -8,15 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    var myDict: [String: String] = ["이름": "이시우", "나이": "23", "취미": "SwiftUI 학습"]
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(myDict["이름"] ?? "")
+            Text(myDict["나이"] ?? "")
+            Text(myDict["취미"] ?? "")
         }
         .padding()
+        .frame(width: 500, height: 300)
+        .onAppear(){
+            print("Hello World!")
+        }
     }
+    
 }
 
 #Preview {
