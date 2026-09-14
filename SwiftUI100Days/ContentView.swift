aa3a4ca8da4bedaa4322a31a328ed856a8372640
Schro2109 <bloodparty297@gmail.com>
2026-09-14 14:47:24 +0900

    feat: SwiftUI 학습용 프로젝트 생성

---
+++ SwiftUI100Days/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI100Days
+//
+//  Created by Schro on 9/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI100Days/SwiftUI100DaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI100DaysApp.swift
+//  SwiftUI100Days
+//
+//  Created by Schro on 9/14/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI100DaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
