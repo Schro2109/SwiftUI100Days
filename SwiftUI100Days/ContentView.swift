85f81dcc869f1ae7b437c2a6c6eddc6e2267abe4
Schro2109 <bloodparty297@gmail.com>
2026-09-14 16:08:44 +0900

    add: Set

--- SwiftUI100Days/ContentView.swift
+++ SwiftUI100Days/ContentView.swift
@@ -9,20 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var myDict: [String: String] = ["이름": "이시우", "나이": "23", "취미": "SwiftUI 학습"]
+    var mySet: Set<String> = ["한국어", "English","日本語"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text(myDict["이름"] ?? "")
-            Text(myDict["나이"] ?? "")
-            Text(myDict["취미"] ?? "")
+        HStack{
+            VStack {
+                Text("Dictionary")
+                Text(myDict["이름"] ?? "")
+                Text(myDict["나이"] ?? "")
+                Text(myDict["취미"] ?? "")
+            }
+            .padding()
+            .onAppear(){
+                print("Hello World!")
+            }
+            VStack{
+                Text("DataSet")
+                ForEach(mySet.sorted().reversed(), id: \.self) { item in
+                    Text(item)
+                }
+                
+            }.padding()
+                .onAppear(){
+                    print("Hello World!")
+                }
         }
-        .padding()
-        .frame(width: 500, height: 300)
-        .onAppear(){
-            print("Hello World!")
-        }
+        
     }
     
 }
